2f57e882ad7fc25ccf89194cd0c28623b151156d
griec <tgh8wp@virginia.edu>
2023-05-03 02:42:19 -0400

    databaseCreation finished

--- src/main/java/edu/virginia/cs/DataBaseCreation.java
+++ src/main/java/edu/virginia/cs/DataBaseCreation.java
@@ -9,10 +9,14 @@ public class DataBaseCreation {
     static Connection connection;
     public static void main(String[] args){
         initializeDatabase();
-        Review nReview = new Review(123,"tgh8wp","hello","yessir",4);
-        addReviewtoTable(nReview);
-        verifyConnection();
-        System.out.print(nReview);
+        Review r = new Review(12121,"tgh8wp","3140","sucks balls",1);
+        Review r2 = new Review(12121,"yassqueen","3140","sucks less balls",4);
+
+        addReviewtoTable(r);
+        addReviewtoTable(r2);
+
+        printReviewsForCourse("3140");
+        printAverageReviewScoreForCourse("3140");
 
     }
     public static void initializeDatabase() {
@@ -77,9 +81,9 @@ public class DataBaseCreation {
                     "(ID INTEGER PRIMARY KEY AUTOINCREMENT,LOGIN VARCHAR(255) not null unique,PASSWORD VARCHAR(255) not null)";
             String coursesQuery = "CREATE TABLE IF NOT EXISTS  COURSES " +
                     "(ID INTEGER PRIMARY KEY AUTOINCREMENT,DEPARTMENT VARCHAR(255) not null,CATALOG_NUMBER INT(4) not null)";
-            stmt.execute(reviewsQuery);
             stmt.execute(studentsQuery);
             stmt.execute(coursesQuery);
+            stmt.execute(reviewsQuery);
             // Close resources
             stmt.close();
         } catch (SQLException e) {
@@ -128,7 +132,146 @@ public class DataBaseCreation {
         }
 
 
+
     }
+    public static boolean courseInDatabase(int catalog, String dept){
+        verifyConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement("select * from COURSES where DEPARTMENT = ? and CATALOG_NUMBER = ?");
+            statement.setString(1, dept);
+            statement.setInt(2, catalog);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void addCourseToTable(Course course){
+        verifyConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO COURSES (ID, DEPARTMENT, CATALOG_NUMBER) VALUES (?, ?, ?)");
+            statement.setInt(1, course.getID());
+            statement.setString(2, course.getDepartment());
+            statement.setInt(3, course.getCatalog());
+            statement.executeUpdate();
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public static void addStudentToTable(Student student){
+        verifyConnection();
+        try {
+            PreparedStatement checkStmt = connection.prepareStatement("SELECT * FROM STUDENTS WHERE ID = ?");
+            checkStmt.setInt(1, student.getID());
+            ResultSet rs = checkStmt.executeQuery();
+            if (rs.next()) {
+                System.out.println("A student with the same ID already exists in the database.");
+                return;
+            }
+            PreparedStatement insertStmt = connection.prepareStatement("INSERT INTO STUDENTS (ID, LOGIN, PASSWORD) VALUES (?, ?, ?)");
+            insertStmt.setInt(1, student.getID());
+            insertStmt.setString(2, student.getLogin());
+            insertStmt.setString(3, student.getPassword());
+            insertStmt.executeUpdate();
+            System.out.println("Student added to database.");
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public static boolean alreadyReviewed(int courseID, int studentID){
+        verifyConnection();
+        try {
+            PreparedStatement checkStmt = connection.prepareStatement("SELECT * FROM REVIEWS WHERE STUDENTID = ?");
+            checkStmt.setInt(1, studentID);
+            ResultSet rs = checkStmt.executeQuery();
+            while (rs.next()) {
+                if(rs.getInt("courseID")==courseID){
+                    return true;
+                }
+            }
+            return false;
+
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public static boolean courseAlreadyHasReviews(String catalog, String dept){
+        verifyConnection();
+        try {
+            String x = courseID(catalog,dept);
+            PreparedStatement checkStmt = connection.prepareStatement("SELECT * FROM REVIEWS WHERE COURSEID = ?");
+            checkStmt.setString(1, x);
+            ResultSet rs = checkStmt.executeQuery();
+            if (rs.next()) {
+                    return true;
+            }
+            return false;
+
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public static String courseID(String catalog, String dept){
+        verifyConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM COURSES WHERE DEPARTMENT = ? and CATALOG_NUMBER = ?");
+            statement.setString(1, dept);
+            statement.setString(2, catalog);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                    return rs.getString("ID");
+            }
+            return null;
+
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public static void printReviewsForCourse(String courseID) {
+        verifyConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT REVIEW_MESSAGE FROM REVIEWS WHERE COURSEID = ?"
+            );
+            statement.setString(1, courseID);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("REVIEW_MESSAGE"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void printAverageReviewScoreForCourse(String courseID) {
+        verifyConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT AVG(RATING) AS AVERAGE_SCORE FROM REVIEWS WHERE COURSEID = ?"
+            );
+            statement.setString(1, courseID);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                double averageScore = rs.getDouble("AVERAGE_SCORE");
+                System.out.printf("Course Average %.2f/5%n", averageScore);
+            } else {
+                System.out.println("No reviews found for this course.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,9 @@
-module hw7.coursereview.tgh8wp.qdy5bq.yqy6yn.main {
+module CourseReviewGUI.main {
     requires java.sql;
     requires org.junit.jupiter.api;
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
-    //requires javafx.controls;
-    //requires javafx.fxml;
-
-    //opens edu.virginia.cs.gui to javafx.fxml;
-    //exports edu.virginia.cs.gui;
+    opens edu.virginia.cs.gui to javafx.fxml;
+    exports edu.virginia.cs.gui;
 }
